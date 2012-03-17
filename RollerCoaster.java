dc1a5cfc533922565d0b8be1442574cf45a9e554
Gareth G. Aye <gareth@gareth.es>
2012-03-17 13:12:32 -0400

    Adding skeleton for RollerCoaster impl

---
+++ RollerCoaster.java
@@ -0,0 +1,33 @@
+package roller_coaster;
+
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+
+public class RollerCoaster implements GLEventListener {
+
+	@Override
+	public void init(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void display(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width,
+			int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
